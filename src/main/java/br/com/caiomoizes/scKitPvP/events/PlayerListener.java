690aa92601a3c56e9b4117b690b7c293322c1897
Caio Moizés Santos <138253578+engcaiomoizes@users.noreply.github.com>
2026-03-09 06:37:37 -0300

    Sponge jump action

--- src/main/java/br/com/caiomoizes/scKitPvP/events/PlayerListener.java
+++ src/main/java/br/com/caiomoizes/scKitPvP/events/PlayerListener.java
@@ -3,10 +3,15 @@ package br.com.caiomoizes.scKitPvP.events;
 import br.com.caiomoizes.scKitPvP.SCKitPvP;
 import br.com.caiomoizes.scKitPvP.player.PlayerData;
 import br.com.caiomoizes.scKitPvP.player.PlayerDataManager;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.util.Vector;
 
 public class PlayerListener implements Listener {
     private final SCKitPvP plugin;
@@ -29,5 +34,19 @@ public class PlayerListener implements Listener {
         PlayerData data = PlayerDataManager.getPlayerData(e.getPlayer());
         SCKitPvP.getInstance().getDbManager().savePlayerData(data);
         PlayerDataManager.removeData(e.getPlayer()); // Limpa da memória RAM
+    }
+
+    @EventHandler
+    public void onSponge(PlayerMoveEvent e) {
+        if (e.getFrom().getBlockX() == e.getTo().getBlockX() &&
+                e.getFrom().getBlockY() == e.getTo().getBlockY() &&
+                e.getFrom().getBlockZ() == e.getTo().getBlockZ()) {
+            return;
+        }
+
+        Block blockBelow = e.getTo().getBlock().getRelative(BlockFace.DOWN);
+        if (blockBelow.getType() == Material.SPONGE) {
+            e.getPlayer().setVelocity(new Vector(0, 1.5, 0));
+        }
     }
 }
